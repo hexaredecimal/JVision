0cfef75897ae5fae923d3e5b1fb000a25e39f6dd
hexaredecimal <mfanakagama@gmail.com>
2024-03-24 09:30:03 +0200

    Helpers: Added methods for extracting paths from filenames and for getting the current time as a string

--- src/jvision/Helpers.java
+++ src/jvision/Helpers.java
@@ -5,6 +5,7 @@
 package jvision;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import jexer.TApplication;
 import jexer.menu.TMenu;
 
@@ -30,4 +31,36 @@ public class Helpers {
 	}
 
 	public static String startDir() { return System.getProperty("user.dir"); }
+	public static String extractPath(String filename) {
+		String[] splits = filename.split("/");
+		String path = ""; 
+		for (int i = 0; i < splits.length; ++i) {
+			if (i != splits.length -1) {
+				path += splits[i] + "/"; 
+			}
+		}
+		return path;
+	}
+
+	public static String getTime() {
+		Calendar c = Calendar.getInstance(); 
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);
+		int seconds = c.get(Calendar.SECOND);
+
+		String hour_str = "" + hour; 
+		String minute_str = "" + minute; 
+		String seconds_str = "" + seconds; 
+
+		if (hour < 10)
+			hour_str = "0" + hour; 
+		if (minute < 10)
+			minute_str = "0" + minute; 
+		if (seconds < 10)
+			seconds_str = "0" + seconds; 
+		
+		String time = hour_str + ":" + minute_str + ":" + seconds_str;
+		return time;
+	}
+
 }
